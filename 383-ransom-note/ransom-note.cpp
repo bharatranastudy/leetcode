b226b2e3a54104dc086c07b1f74debcc87450d15
Bharat rana <krishnathakur90156@gmail.com>
2026-07-21 14:00:33 +0530

    Time: 6 ms (26.02%) | Memory: 11.7 MB (75.10%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int> freq(26, 0);
+
+        // Count letters in magazine
+        for (char ch : magazine) {
+            freq[ch - 'a']++;
+        }
+
+        // Use letters for ransomNote
+        for (char ch : ransomNote) {
+            if (freq[ch - 'a'] == 0)
+                return false;
+
+            freq[ch - 'a']--;
+        }
+
+        return true;
+    }
+};
