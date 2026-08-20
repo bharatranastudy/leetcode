00db23b0485bf6fc470d9a98e002e99c6583c49d
Bharat rana <krishnathakur90156@gmail.com>
2026-08-20 10:22:35 +0530

    Time: 18 ms (97.97%) | Memory: 16.1 MB (90.35%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,40 +1,39 @@
 class Solution {
 public:
 
-    void dfs(vector<vector<char>>& grid, int i, int j) {
-        int n = grid.size();
+
+
+
+void dfs(vector<vector<char>>& grid,int i, int j){
+   int n = grid.size();
         int m = grid[0].size();
 
-        // Boundary check + water check
-        if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] == '0') {
-            return;
+        if(i<0||j<0||i>=n||j>=m||grid[i][j] == '0'){
+            return ;
         }
 
-        // Mark current land as visited
         grid[i][j] = '0';
 
-        // Explore 4 directions
-        dfs(grid, i + 1, j); // down
-        dfs(grid, i - 1, j); // up
-        dfs(grid, i, j + 1); // right
-        dfs(grid, i, j - 1); // left
-    }
+        dfs(grid , i+1,j);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i,j-1);
 
+
+
+}
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
         int count = 0;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-
-                if (grid[i][j] == '1') {
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(grid[i][j] == '1'){
                     count++;
-                    dfs(grid, i, j);
+                    dfs(grid,i,j);
                 }
             }
         }
-
         return count;
     }
 };
